17cf9b88e67460b27bdc3840b5f2aa5f3f1b5e3b
Ron <ronaddriel@Rons-MacBook-Pro.local>
2025-05-11 23:25:47 -0400

    Third Commit - Added routes for Book Operation

--- book-tracker/index.js
+++ book-tracker/index.js
@@ -29,3 +29,31 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+// Add a route to get all books
+app.get('/books', async (req, res) => {
+    try {
+        const books = await Book.find();
+        res.json(books);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+}   );
+// Add a route to add a new book
+app.post('/books', async (req, res) => {
+    try{
+        const{title, author, status, notes, dateFinished} = req.body;
+        const book = new Book({
+            title,
+            author,
+            status,
+            notes,
+            dateFinished: dateFinished || null,
+        });
+        await newBook.save();
+        res.status(201).json(newBook);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+);
